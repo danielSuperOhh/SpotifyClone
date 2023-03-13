820a169cc81a1955ea15685f44ce98cca12363a9
danielSuperOhh <dannyroycevita@gmail.com>
2023-03-13 23:05:30 +0000

    update

--- src/utils/api/authorize.js
+++ src/utils/api/authorize.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "ac3ee6f017b54a0d8e1e7935083cc0da";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://beemusicdaniel.netlify.app/";
 const scopes = ["user-read-currently-playing", "user-read-playback-state", "user-library-read"];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
